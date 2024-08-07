c2c6af61b1086c69d079a0977848a0a4d4ae2074
Indrajeet2809 <indrajeetraj2809@gmail.com>
2024-08-08 00:13:01 +0530

    first commi

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your Favourite Food Here</h2>
+        <p>Choose your food from the diverse food option</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
